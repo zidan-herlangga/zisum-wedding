94fa122f0487efadb54d96754882f8a57184c140
Zidan Herlangga <133259343+zidanherlangga04@users.noreply.github.com>
2023-08-11 11:16:55 +0700

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 /* ========================================================================= */
-/* ========================================================================= */
-/* ========================================================================= */
+// Smooth Scroll
+// var scroll = new SmoothScroll('a[href*="#"]');
 
+/* ========================================================================= */
 // Navbar Hidden or Visible
 const hamburger = document.querySelector(".navbar-toggler");
 const stickyTop = document.querySelector(".sticky-top");
@@ -17,13 +18,59 @@ offcanvas.addEventListener("hidden.bs.offcanvas", function () {
 });
 
 /* ========================================================================= */
-/* ========================================================================= */
-/* ========================================================================= */
+// Backsound Music
+var track = document.getElementById("track");
+var controlBtn = document.getElementById("play-pause");
+
+function playPause() {
+  if (track.paused) {
+    track.play();
+    //controlBtn.textContent = "Pause";
+    controlBtn.className = "pause";
+  } else {
+    track.pause();
+    //controlBtn.textContent = "Play";
+    controlBtn.className = "play";
+  }
+}
+
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function () {
+  controlBtn.className = "play";
+});
+
+// Backsound Website
+function playSound(audioName, loop, pause) {
+  let audio = new Audio(audioName);
+  audio.pause = false;
+  audio.loop = true;
+  audio.play();
+}
+playSound(".mp3");
 
 /* ========================================================================= */
-/* ========================================================================= */
-/* ========================================================================= */
+// RSVP
+function gotoWhatsapp() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
 
+  let url =
+    "https://wa.me/62xxxxxxxxxxx?text=" + // Masukan no whatsapp kalian agar para tamu dapat mengirim pesan
+    "Nama : " +
+    name +
+    "%0a" +
+    +"Email : " +
+    email +
+    "%0a";
+  "Phone : " + phone + "%0a" + "%0a" + "Pesan : " + "%0a" + message;
+
+  window.open(url, "_black").focus();
+}
+
+/* ========================================================================= */
+// CopyText-Gifts
 let copyText = document.querySelector(".copy-text");
 copyText.querySelector("button").addEventListener("click", () => {
   let input = document.querySelector("input.text");
